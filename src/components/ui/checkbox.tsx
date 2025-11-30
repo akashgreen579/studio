c16ff0129ecf629958121afbbdd621426507adee
AKASH P <akashgreen2468@gmail.com>
2025-11-30 03:58:06 +0000

    Try fixing this error: `Console Error: Received `false` for a non-boolea

--- src/components/ui/checkbox.tsx
+++ src/components/ui/checkbox.tsx
@@ -11,22 +11,7 @@ const Checkbox = React.forwardRef<
   React.ElementRef<typeof CheckboxPrimitive.Root>,
   React.ComponentPropsWithoutRef<typeof CheckboxPrimitive.Root>
 >(({ className, checked, ...props }, ref) => {
-  const localRef = React.useRef<HTMLButtonElement>(null);
-  
-  const indeterminate = typeof checked !== 'boolean' && checked?.valueOf() === 'indeterminate';
-
-  React.useEffect(() => {
-    if (localRef.current) {
-        localRef.current.dataset.state = indeterminate ? 'indeterminate' : (checked ? 'checked' : 'unchecked');
-    }
-  }, [indeterminate, checked]);
-
-  React.useEffect(() => {
-    if (localRef.current) {
-      localRef.current.indeterminate = indeterminate ?? false;
-    }
-  }, [indeterminate]);
-
+  const isIndeterminate = checked === "indeterminate";
 
   return (
     <CheckboxPrimitive.Root
@@ -35,7 +20,7 @@ const Checkbox = React.forwardRef<
         "peer h-4 w-4 shrink-0 rounded-sm border border-primary ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-primary data-[state=checked]:text-primary-foreground data-[state=indeterminate]:bg-primary data-[state=indeterminate]:text-primary-foreground",
         className
       )}
-      checked={checked}
+      checked={isIndeterminate ? "indeterminate" : checked}
       {...props}
     >
       <CheckboxPrimitive.Indicator
@@ -49,5 +34,3 @@ const Checkbox = React.forwardRef<
 Checkbox.displayName = CheckboxPrimitive.Root.displayName
 
 export { Checkbox }
-
-    
